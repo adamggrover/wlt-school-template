6b9c0a8136626af8f28ed80ed478ce2fc6c70ce5
Adam Grover <adamggrover@gmail.com>
2023-05-19 14:37:10 +0100

    remove google tag

--- header.php
+++ header.php
@@ -8,14 +8,7 @@
     
 
     <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-B0NFPDKC8G"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
 
-      gtag('config', 'G-B0NFPDKC8G');
-    </script>
 
 
 
