47bc17ab862620488c616180830f33c085db360e
Adam Grover <adamggrover@gmail.com>
2023-05-19 15:33:25 +0100

    put content-header in seperate file and include

--- 404.php
+++ 404.php
@@ -4,15 +4,7 @@
 
 
 <div class="container" id="main-page-area" data-aos="fade-up" data-aos-delay="200" data-aos-duration='800' >
-            <div class="content-header row gx-3">
-                <div class="content-header-breadcrumb-wrapper col-lg-6 col-md-12">
-                    <i class="fa-solid fa-house fa-xl"></i><span class="breadcrumb-spacer"></span>
-                    <h6 class="mb-0 content-header-breadcrumb-links"><small><?php if( function_exists( 'aioseo_breadcrumbs' ) ) aioseo_breadcrumbs(); ?></small></h6>
-                    
-
-                </div>
-   
-            </div>
+<?php include 'content-header.php';?>
             <div class="content-area row" >
                 <div class="col-lg-12 content-column">
                     
--- single-jobadverts.php
+++ single-jobadverts.php
@@ -3,25 +3,7 @@
 ?>
 
 <div class="container" id="single-post-main-page-area" data-aos="fade-up" data-aos-delay="200" data-aos-duration='800' >
-    <div class="content-header row gx-3">
-                    <div class="content-header-breadcrumb col-lg-6 col-md-12">
-                        <i class="fa-solid fa-house fa-xl"></i><span class="breadcrumb-spacer"></span>
-                        <h6 class="mb-0 content-header-breadcrumb-links"><small><?php if( function_exists( 'aioseo_breadcrumbs' ) ) aioseo_breadcrumbs(); ?></small></h6>
-                        
-
-                    </div>
-                    <div class="content-header-social col-lg-6 col-md-12">
-                    <h6 class="mb-0"><small>Share This Page</small></h6><span class="breadcrumb-spacer"></span>
-                        <div class="content-area-social-links">
-                            <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" target="_blank" data-show-count="false"><i class="fa-brands fa-twitter fa-sm ps-2 fa-xl"></i></a>
-                            <a href="https://www.facebook.com/sharer/sharer.php?u=example.org" target="_blank"><i class="fa-brands fa-facebook-f ps-2 fa-xl"></i></a>
-                            <a href="mailto:office@weareacademy.co.uk"><i class="fa-solid fa-envelope fa-sm ps-2 fa-xl"></i></a>   
-
-                        </div>
-                        
-                        
-                    </div>
-                </div>
+<?php include 'content-header.php';?>
 
             <div class="content-area row gx-3">
                 <div id="post-content-column col-12">
