99d6ec1d8681fb227137708c619fcf0fc5fc8d3d
Adam Grover <adamggrover@gmail.com>
2023-05-25 10:25:33 +0100

    change page title to wp site title

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Weare Academy First School and Rainbow Woods Pre-School</title>
+    <title><?php echo get_bloginfo('name'); ?></title>
     
 
     <!-- Google tag (gtag.js) -->
