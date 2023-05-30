ecc06bf48638b6d37eb142b901fa9d5025bb3c04
Adam Grover <adamggrover@gmail.com>
2023-05-30 15:40:23 +0100

    change header change func to css color variable

--- src/js/main.js
+++ src/js/main.js
@@ -159,7 +159,7 @@ $(window).on('load', function() { // makes sure the whole site is loaded
 
 function headerBackground() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("header").style.backgroundColor = "#2D4190";
+    document.getElementById("header").style.backgroundColor = "var(--primary)";
   } else {
     document.getElementById("header").style.backgroundColor = "transparent";
   }
