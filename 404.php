c01fbea9f96d89294ffa7839c11e38e37491bc3f
Adam Grover <adamggrover@gmail.com>
2023-05-31 11:08:36 +0100

    change 404 to redirect to home page

--- 404.php
+++ 404.php
@@ -1,44 +1,5 @@
 <?php
-    get_header();
+    wp_redirect( get_home_url()  );
     ?>
 
 
-<div class="container" id="main-page-area" data-aos="fade-up" data-aos-delay="200" data-aos-duration='800' >
-<?php include 'content-header.php';?>
-            <div class="content-area row" >
-                <div class="col-lg-12 content-column">
-                    
-                    <h1 id="content-h1">Page Not Found</h1>
-
-                    <div class="content-body row">
-
-
-                    <h2><strong>404</strong></h2>                  
-                      <p>Sorry, the page you are looking for might have been removed, had its name changed or is temporarily unavailable.</p>
-
-
-                       
-
-                        </div>
-
-                        
-
-
-
-                    </div>
-                </div>
-
-
-                    </div>
-
-                </div>
-            </div>
-        </div>
-       
-
-
-
-
-<?php
-    get_footer();
-    ?>
