e2ddfedeb30b1b5e4868ab68fb505a9dca347956
Adam Grover <adamggrover@gmail.com>
2023-06-02 15:16:37 +0100

    and if func exists for custom logo

--- footer.php
+++ footer.php
@@ -13,9 +13,11 @@ $query = new WP_Query( array( 'page_id' => get_option('page_on_front')) ); ?>
         <div class="footer-content container pb-2">
             <div class="row footer-logo">
                 <div class="col-md-4">
+                <?php if( function_exists('custom_logo')) { ?>
                     <img src="<?php $custom_logo_id = get_theme_mod( 'custom_logo' );
 $image = wp_get_attachment_image_src( $custom_logo_id , 'full' );
 echo $image[0]; ?>" alt="Weare Academy Logo" class="footer-icon">
+<?php }else{} ?>
                 </div>
 
             </div>
--- header-front.php
+++ header-front.php
@@ -41,9 +41,16 @@
                 <div class="logo-container">
                     <a class="home-logo-link" href="<?php echo get_home_url(); ?>">
                         <div alt="Logo" class="logo-content logo-front-page img-fluid">
+
+                        <?php if( function_exists('custom_logo')) { ?>
+
                             <img class="logo-img" src="<?php $custom_logo_id = get_theme_mod( 'custom_logo' );
                                 $image = wp_get_attachment_image_src( $custom_logo_id , 'full' );
                                     echo $image[0]; ?>"></img>
+
+                        <?php }else{} ?>
+
+                            
                             <h1 class="mb-2 h3 logo-heading"><?php echo get_bloginfo('name'); ?></h1>
                         </div>
                     </a>
