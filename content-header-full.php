72065941c729b0434fb987153e00a892f58b4f77
Adam Grover <adamggrover@gmail.com>
2023-06-05 13:05:26 +0100

    increase gutter

--- content-header-full.php
+++ content-header-full.php
@@ -1,4 +1,4 @@
-<div class="content-header row gx-3">
+<div class="content-header row gx-5">
                 <div class="content-header-breadcrumb-wrapper col-lg-6 col-md-12">
                     <i class="fa-solid fa-house fa-xl"></i><span class="breadcrumb-spacer"></span>
                     <h6 class="mb-0 content-header-breadcrumb-links p-0"><small><?php if( function_exists( 'aioseo_breadcrumbs' ) ) aioseo_breadcrumbs(); ?></small></h6>
--- page.php
+++ page.php
@@ -19,7 +19,7 @@
 <div class="container" data-aos="fade-up" data-aos-delay="200" data-aos-duration='800' id="main-page-area">
 <?php include 'content-header.php';?>
 
-            <div class="content-area row gx-3">
+            <div class="content-area row gx-5">
                 <div class="col-lg-9 content-column">
                     
                     <h1 id="content-h1"><?php the_title(); ?></h1>
