65d0d26a74e2b8a11bf6dbfac3d3987cee4e0c07
Adam Grover <adamggrover@gmail.com>
2023-06-07 11:39:46 +0100

    add if statement to display file block count

--- archive-key-information-layout.php
+++ archive-key-information-layout.php
@@ -39,8 +39,16 @@
                 <div class="key-information-item-content">
 
                     <h2 class="key-information-title h5"><?php the_title();?></h2>
+
+                    <?php 
+                           if ($file_block_count > 0) {
+                            ?>
+                            <div class="key-information-icon-wrapper"><div><i class="fa-solid fa-file"></i></div><span></span><p class="file-count"><?php echo $file_block_count;?></p></div>
+                        <?php
+                        
+                        }else{}
             
-                    <div class="key-information-icon-wrapper"><div><i class="fa-solid fa-file"></i></div><span></span><p class="file-count"><?php echo $file_block_count;?></p></div>
+                    ?>
                     
                     
                 </div>
