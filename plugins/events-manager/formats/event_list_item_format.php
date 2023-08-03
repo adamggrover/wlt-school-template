9fd349c33f40f63ab69fd35e582bb249ba84e3a9
Adam Grover <adamggrover@gmail.com>
2023-08-03 15:54:08 +0100

    add event format overide files

---
+++ plugins/events-manager/formats/event_list_item_format.php
@@ -0,0 +1,38 @@
+
+<div class="em-event em-item" style="--default-border:#_CATEGORYCOLOR;">
+
+	<div class="date">
+					<span class="day h4">#d</span>
+					<span class="month h6">#M</span>
+	</div>
+
+	<div class="em-item-info">
+
+		<div class="event-item-info-inner">
+		
+
+			<div class="event-info">
+				<h5 class="em-item-title">#_EVENTNAME</h5>
+				<div class="time-container pt-1 ">
+					<small><i class="fa-regular fa-clock text-muted"></i></small>
+					<h6 class="text-muted"><small>#_EVENTTIMES</small></h6>
+
+				</div>
+			</div>
+
+			<!--
+			<div class="calendar-subscribe-item-container">
+				<i class="fa-regular fa-calendar-days"></i>
+				
+			</div>
+-->
+
+		</div>
+			
+
+			
+
+		
+				
+	</div>
+</div>
---
+++ plugins/events-manager/templates/events-list-grouped.php
@@ -0,0 +1,51 @@
+<?php
+/*
+ * Default Events List Template
+ * This page displays a list of events, called during the em_content() if this is an events list page.
+ * You can override the default display settings pages by copying this file to yourthemefolder/plugins/events-manager/templates/ and modifying it however you need.
+ * You can display events however you wish, there are a few variables made available to you:
+ * 
+ * $args - the args passed onto EM_Events::output()
+ * 
+ */
+$args = apply_filters('em_content_events_args', $args);
+if( empty($args['id']) ) $args['id'] = rand(); // prevent warnings
+$id = esc_attr($args['id']);
+?>
+
+<div id="calendar-subscribe-link-container" class="mb-0">
+	<i class="fa-regular fa-calendar-days"></i>
+	<p>Subscribe to full calendar</p>
+	
+</div>
+
+<!-- get ical feed url -->
+<?php 
+		
+		$site_url = get_home_url(); 
+		$url = preg_replace("(^https?)", "webcal", $site_url ); 
+		
+?>
+
+<div id="cal-feed-instructions-container">
+	<div class="cal-feed-link-container">
+
+		<div class="cal-feed-instructions">
+			<p>Please find below instructions on how to configure some of the most popular calendar programmes to automatically display dates from our calendar using the address below:</p>
+			<p><small><?php echo $url . "/events.ics"; ?></small></p>
+			<p>Copy the URL below and click the device type below for further instructions:</p>
+
+		</div>
+	</div>
+</div>
+
+
+
+
+<div class="<?php em_template_classes('view-container'); ?>" id="em-view-<?php echo $id; ?>" data-view="list-grouped">
+	<div class="<?php em_template_classes('events-list', 'events-list-grouped'); ?>" id="em-events-list-grouped-<?php echo $id; ?>">
+	<?php
+	echo EM_Events::output_grouped($args); //note we're grabbing the content, not em_get_events_list_grouped function
+	?>
+	</div>
+</div>
