b57e9f29442c4634b2ccf029bd6ccde2c33cf146
Adam Grover <adamggrover@gmail.com>
2023-10-12 10:31:53 +0100

    remove text area subheading

--- flexible-content/text.php
+++ flexible-content/text.php
@@ -1,13 +1,8 @@
 <?php
-// Display subheading
 
-$heading = $args['heading'];
 $textArea = $args['text_area'];
 
-
-if( $heading): ?>
-    <h2><?php echo $heading ?></h2>
-<?php endif;?>
+?>
 
 <div class='mb-5'><?php
 echo $textArea;?>
