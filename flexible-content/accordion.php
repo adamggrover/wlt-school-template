1e5ac3935aa4bc8e47dc7bb0434893b6f9879a2a
Adam Grover <adamggrover@gmail.com>
2023-10-13 14:07:08 +0100

    update accordion styling

--- flexible-content/accordion.php
+++ flexible-content/accordion.php
@@ -10,16 +10,20 @@
 
     foreach ($accordionItems as $accordionItem) :  ?>
 
-      <div class="accordion-item mb-0" id="accordionFlushExample<?php echo $collapseId ?>">
-        <h2 class="accordion-header mb-0" id="flush-heading<?php echo $collapseId ?>">
+    
+
+      <div class="accordion-item" id="accordionFlushExample<?php echo $collapseId ?>">
+        <h2 class="accordion-header" id="flush-heading<?php echo $collapseId ?>">
           <button class="accordion-button collapsed mb-0" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse<?php echo $collapseId ?>" aria-expanded="false" aria-controls="flush-collapse<?php echo $collapseId ?>">
           <?php echo $accordionItem['accordion_item_title'] ?>
           </button>
         </h2>
         <div id="flush-collapse<?php echo $collapseId ?>" class="accordion-collapse collapse" aria-labelledby="flush-heading<?php echo $collapseId ?>" data-bs-parent="#accordionFlushExample<?php echo $collapseId ?>">
-          <div class="accordion-body mb-0"><?php echo $accordionItem['accordion_item_body'] ?></div>
+          <div class="accordion-body"><?php echo $accordionItem['accordion_item_body'] ?></div>
         </div>
       </div>
+
+
       <?php
 
       $collapseId++;
