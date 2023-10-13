b63403ac8a1cb677ad290be89268d3f5bc3007c2
Adam Grover <adamggrover@gmail.com>
2023-10-13 10:23:05 +0100

    add accordion to custom page builder

---
+++ flexible-content/accordion.php
@@ -0,0 +1,31 @@
+<div class="accordion accordion-flush" id="accordionFlushExample">
+
+<?php
+
+  $accordionItems = $args['accordion_items'];
+
+  if( $accordionItems): 
+
+    $collapseId = 0;
+
+    foreach ($accordionItems as $accordionItem) :  ?>
+
+      <div class="accordion-item mb-0" id="accordionFlushExample<?php echo $collapseId ?>">
+        <h2 class="accordion-header mb-0" id="flush-heading<?php echo $collapseId ?>">
+          <button class="accordion-button collapsed mb-0" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse<?php echo $collapseId ?>" aria-expanded="false" aria-controls="flush-collapse<?php echo $collapseId ?>">
+          <?php echo $accordionItem['accordion_item_title'] ?>
+          </button>
+        </h2>
+        <div id="flush-collapse<?php echo $collapseId ?>" class="accordion-collapse collapse" aria-labelledby="flush-heading<?php echo $collapseId ?>" data-bs-parent="#accordionFlushExample<?php echo $collapseId ?>">
+          <div class="accordion-body mb-0"><?php echo $accordionItem['accordion_item_body'] ?></div>
+        </div>
+      </div>
+      <?php
+
+      $collapseId++;
+
+    endforeach; 
+
+    endif; ?>
+
+</div>
