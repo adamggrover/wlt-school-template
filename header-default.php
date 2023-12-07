08924c19419cd5dabb333f3112b39805b5c41474
Adam Grover <adamggrover@gmail.com>
2023-12-07 16:04:14 +0000

    update search close icon to bootstrap version

--- header-default.php
+++ header-default.php
@@ -13,7 +13,7 @@
                 </div>
                 <div class="search-right">
                     <button onclick="closeSearch()">
-                        <i class="fa-solid fa-xmark fa-lg mb-3"></i>                        
+                        <i class="bi bi-x-lg"></i>                        
                     </button>
                 </div>
             </div>
