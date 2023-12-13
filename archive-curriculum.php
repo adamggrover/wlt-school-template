7a9a0e66eb072ebfd329aabc4e57056dcf8d0a92
Adam Grover <adamggrover@gmail.com>
2023-12-13 11:56:47 +0000

    add page builder to page

--- archive-curriculum.php
+++ archive-curriculum.php
@@ -21,7 +21,166 @@
                         $query->the_post(); ?>
 
                 <div class="content-area row gx-5 wordpress-page-area mb-5 content-column content-body">
-                    <?php the_content(); ?>
+                    <?php the_content(); 
+
+                    //-----------------Page Builder-------------------------------------------------------------
+
+
+
+
+                            // Check value exists.
+                            if( have_rows('page_builder') ):
+
+                                                       
+
+
+                           
+
+                                    // ACF - Flexible Content fields.
+                                    $sections = get_field( 'page_builder' );
+
+                                   
+
+                                    if ( $sections ) :
+                                        foreach ( $sections as $section ) :
+                                            $template = str_replace( '_', '-', $section['acf_fc_layout'] );
+                                            get_template_part( 'flexible-content/' . $template, '', $section );
+                                        endforeach;
+                                    endif;
+
+                          
+                       
+
+
+                            endif;
+
+                        // -----------------File Upload Section-----------------------------------------------
+                        //check if page has file-upload acf fields and display them if it does
+
+                        if( have_rows('file_section') ):
+                            while( have_rows('file_section') ) : the_row();
+
+
+                            // Display subheading
+                            if( get_sub_field('file_subheading') ): ?>
+                                <h3><?php the_sub_field('file_subheading'); ?></h3>
+                                <?php endif; 
+
+
+
+                                // Loop over sub repeater rows.
+                                if( have_rows('file_upload_section') ):
+                                    while( have_rows('file_upload_section') ) : the_row();
+
+
+
+                                        ?>
+
+                                        <!-- Display acf file upload repeater fields -->
+                                        <li class="file-upload-repeater">
+                                            <a href="<?php the_sub_field('file_upload') ?>" target="_blank">
+                                            <?php the_sub_field('file_title') ?>
+                                            </a>
+                                        </li>
+                                            
+                                        <?php
+                                    endwhile;
+                                endif;
+                            endwhile;
+                        endif;
+
+                        
+                        // -----------------Table Upload Section-----------------------------------------------
+                        //check if page has table acf fields and display them if it does
+
+                        if( have_rows('table_section') ):
+                            while( have_rows('table_section') ) : the_row();?>
+
+
+
+
+
+
+
+
+                                    
+                                    <table class="mb-5">
+                                    <?php
+                                    // Display subheading
+
+                                    
+                                    
+                                    
+                                    if( get_sub_field('table_subheading') ): ?>
+                                        <tr>
+                                            <th colspan="2">
+                                                <?php the_sub_field('table_subheading'); ?>
+                                            
+                                            </th>
+                                        </tr>
+                                    <?php endif; ?>
+
+                                    <!-- Works above -->
+                                    <?php
+                                    if( have_rows('table_cells') ): ?>
+                                    <?php while( have_rows('table_cells') ) : the_row();
+
+
+
+                                        ?>
+
+                                        <!-- Display acf repeater fields -->
+                                        <tr>
+                                            <td><?php the_sub_field("table_col_one") ?></td>
+                                            <td><?php the_sub_field("table_col_two") ?></td>
+                                            
+                                        </tr>
+                                            
+                                        <?php
+                                    endwhile;?>
+                                    <?php endif; ?>
+                                    
+                                    <!-- Works below -->
+
+                                    </table>
+
+
+
+                                    <?php
+                              
+                            endwhile;
+                        endif;
+
+                        
+                        //-------------Team Table-------------------------------------------------
+                    
+
+                        if( have_rows("team_table") ): ?>
+                            <table class="mb-5">
+                            <?php if(get_field("team_name") ): ?>
+                                <tr>
+                                    <th colspan="2"><?php the_field('team_name') ?> Team</th>
+                                </tr>
+                            <?php endif; ?>
+                            
+                            <?php while( have_rows("team_table") ) : the_row();
+    
+    
+    
+                                ?>
+    
+                                <!-- Display acf file upload repeater fields -->
+                                
+                                <tr>
+                                    <td><?php the_sub_field("team_member_name") ?></td>
+                                    <td><?php the_sub_field("team_member_position") ?></td>
+                                    
+                                </tr>
+                                    
+                                    <?php
+                                endwhile; ?>
+                                </table>
+                            <?php endif;?>
                 </div>
 
                 <?php }
@@ -50,7 +209,7 @@
 
                     ?>
 
-                    <h3 class="curriculum-cat-heading h3 mt-5 pt-5 mb-5">Curriculum By Subject Area</h3>
+                    <h3 class="curriculum-cat-heading h3 mt-1 pt-0 mb-5">Curriculum By Subject Area</h3>
                     <div class="row row-cols-1 row-cols-md-3 row-cols-lg-4  g-2 mb-5"> 
 
                     <?php
@@ -107,7 +266,7 @@
 
                     ?>
 
-                    <h3 class="curriculum-cat-heading h3 mt-5 pt-5 mb-5">Curriculum By Year Group</h3>
+                    <h3 class="curriculum-cat-heading h3 mt-1 pt-0 mb-5">Curriculum By Year Group</h3>
                     <div class="row row-cols-1 row-cols-md-3 row-cols-lg-4  g-2 mb-5"> 
 
                     <?php
